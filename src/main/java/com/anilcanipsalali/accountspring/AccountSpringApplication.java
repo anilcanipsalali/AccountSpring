f41060d4308e4bf00076963b251a36890139a2e7
anilcanipsalali <anilcan.ipsalali@gmail.com>
2022-05-15 19:19:33 +0300

    Initial commit.

---
+++ src/main/java/com/anilcanipsalali/accountspring/AccountSpringApplication.java
@@ -0,0 +1,12 @@
+package com.anilcanipsalali.accountspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/anilcanipsalali/accountspring/AccountSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilcanipsalali.accountspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
