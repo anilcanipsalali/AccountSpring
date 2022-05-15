4b36730288b28fdd3aecf21cb6314cb251e65f59
anilcanipsalali <anilcan.ipsalali@gmail.com>
2022-05-15 19:24:27 +0300

    Initial commit.

--- src/main/java/com/anilcanipsalali/accountspring/AccountSpringApplication.java
+++ src/main/java/com/anilcanipsalali/accountspring/AccountSpringApplication.java
@@ -1,12 +1,18 @@
 package com.anilcanipsalali.accountspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountSpringApplication {
+public class AccountSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
